e5a4dc7b3604ddb01db796b4145fe660e945bd8b
dut09 <dutao09@gmail.com>
2015-02-16 13:10:10 -0800

    Test plotFeaturePoint.

---
+++ src/plotFeaturePoint.m
@@ -0,0 +1,34 @@
+function [ I ] = plotFeaturePoint( I, p, c )
+% Tao Du
+% taodu@stanford.edu
+% Feb 16, 2015
+%
+% Given an image I, a pixel p, and a color c, plot a cross centered at p.
+%
+% Input: I: a row x column x 3 image. The range is [0, 1].
+%        p: a n x 2 matrix. Each row is a pixel we want to plot.
+%        c: a n x 3 matrix within the range [0, 1]. Each row is the color
+%           we want to use to plot the corresponding pixel in p.
+
+% Get the number of pixels.
+n = size(p, 1);
+
+% Get the size of the image.
+h = size(I, 1);
+w = size(I, 2);
+
+% Set the size of the cross.
+s = 15;
+
+% Plot each feature point.
+for i = 1 : n
+  % Loop over all the three channels.
+  for j = 1 : 3
+    % Clamp it so that it won't be out of bound.
+    I(max(p(i, 1) - s, 1) : min(p(i, 1) + s, h), p(i, 2), j) = c(i, j);
+    I(p(i, 1), max(p(i, 2) - s, 1) : min(p(i, 2) + s, w), j) = c(i, j);
+  end
+end
+
+end
+
---
+++ src/test/testPlotFeaturePoint.m
@@ -0,0 +1,26 @@
+% Tao Du
+% taodu@stanford.edu
+% Feb 16, 2015
+
+% Test plotFeaturePoint.
+% clear;
+clear all; clc;
+
+% Genrate a black image.
+m = 480;
+n = 640;
+I = zeros(m, n, 3);
+imshow(I);
+
+% The number of pixels.
+k = 4;
+
+% Call ginput to select k pixels.
+p = image2Pixel(ginput2Image(ginput(k)));
+
+% Generate random colors.
+c = rand(k, 3);
+
+% Plot the image.
+I = plotFeaturePoint(I, p, c);
+imshow(I);
